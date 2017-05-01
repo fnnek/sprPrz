3c69f10a620696158a538a124e684547fe55b153
fnnek <mariuszfranciszekbozek@gmail.com>
2017-05-01 13:07:36 +0200

    Podstawowe logowanie

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-
+      log_in @user
+      flash[:success] = "Witaj w systemie!"
+      redirect_to @user
     else
       render 'new'
     end
