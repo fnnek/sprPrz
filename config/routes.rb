0bab91b68cf352fbadea77d6626bc99b57cdd2eb
fnnek <mariuszfranciszekbozek@gmail.com>
2017-05-16 16:42:35 +0200

    Remembering users

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resources :users
-  root :to => 'home#index', :constraints => CustomDomainConstraint
+  root :to => 'home#index'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
